242248b45252374436fc57566902896b4f2390b2
Daniil Skokleenko <dcman1993@icloud.com>
2018-01-10 18:31:06 +0300

    adjusted

--- implementations/Pulverizer.py
+++ implementations/Pulverizer.py
@@ -10,16 +10,21 @@ b = int(tokens[1])
 x,y = a,b
 c,d,e,f = (1,0,0,1)
 q = x // y
+print("x: ",x,"y: ",y)
+print("x = ",c,"a", d,"b","y = ", e,"a", f,"b")
+print("q: ", q)
+
 
 while x % y != 0:
-    print("x: ",x,"y: ",y)
-    print("x = ",c,"a", d,"b","y = ", e,"a", f,"b")
-    print("q: ", q)
+
     q = x // y
     x, y = y, x % y
     c,d,e,f = (e, f, c-q * e, d-q * f)
-        #print(c, d, e, f)
+    print("x: ",x,"y: ",y)
+    print("x = ",c,"a", d,"b","y = ", e,"a", f,"b")
+    print("q: ", q)
 
+x, y = y, x
 c, d = e, f
 print("gcd:", x)
 print("s: ", c, "t: ", d)
