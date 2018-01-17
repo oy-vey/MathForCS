4789890278d6bffedfab479c14a81ace38eaa898
Daniil Skokleenko <dcman1993@icloud.com>
2018-01-17 23:30:40 +0300

    Assignments for spring 2015 added

--- fall-2010/implementations/Pulverizer.py
+++ fall-2010/implementations/Pulverizer.py
@@ -12,19 +12,20 @@ c,d,e,f = (1,0,0,1)
 q = x // y
 print("x: ",x,"y: ",y)
 print("x = ",c,"a", d,"b","y = ", e,"a", f,"b")
-print("q: ", q)
 
+step = 1
 
 while x % y != 0:
-
+    print("Step:", step)
     q = x // y
     x, y = y, x % y
     c,d,e,f = (e, f, c-q * e, d-q * f)
+    step += 1
     print("x: ",x,"y: ",y)
     print("x = ",c,"a", d,"b","y = ", e,"a", f,"b")
-    print("q: ", q)
 
 x, y = y, x
 c, d = e, f
-print("gcd:", x)
+print()
+print("gcd:", x, "steps:", step)
 print("s: ", c, "t: ", d)
